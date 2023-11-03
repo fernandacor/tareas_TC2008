8a735423377f5a0c832d0ca45bea061214bd71bf
Fernanda Cantú <fercantu56@gmail.com>
2023-11-02 18:40:05 -0600

    simulacion 2 no sirve todavia

---
+++ Actividad 1/Simulacion 2/agent.py
@@ -0,0 +1,89 @@
+from mesa import Agent
+
+class TreeCell(Agent):
+    """
+        A tree cell.
+        
+        Attributes:
+            x, y: Grid coordinates
+            condition: Can be "Fine", "On Fire", or "Burned Out"
+            unique_id: (x,y) tuple.
+
+            unique_id isn't strictly necessary here, but it's good practice to give one to each agent anyway.
+    """
+
+    def __init__(self, pos, model):
+        """
+        Create a new tree.
+
+        Args:
+            pos: The tree's coordinates on the grid.
+            model: standard model reference for agent.
+        """
+        super().__init__(pos, model) # inicializa como vivo
+        self.pos = pos
+        self.condition = "Alive" 
+        self._next_condition = None
+
+    def step(self):
+        """
+        If the tree is on fire, spread it to fine trees nearby.
+        """
+        if self.condition == "Dead":
+            neighbors = []
+            for neighbor in self.model.grid.iter_neighbors(self.pos, True):
+                if neighbor.pos[1] == self.pos[1] + 1:
+                    neighbors.append(neighbor)
+                    
+            if len(neighbors) == 3:
+                    # 0 0 1
+                if ((neighbors[0].condition == "Dead" and 
+                    neighbors[1].condition == "Dead" and
+                    neighbors[2].condition == "Alive") or
+
+                    # 0 1 1
+                    (neighbors[0].condition == "Dead" and
+                    neighbors[1].condition == "Alive" and
+                    neighbors[2].condition == "Alive") or
+                    
+                    # 1 0 0
+                    (neighbors[0].condition == "Alive" and
+                    neighbors[1].condition == "Dead" and
+                    neighbors[2].condition == "Dead") or
+                    
+                    # 1 1 0
+                    (neighbors[0].condition == "Alive" and
+                    neighbors[1].condition == "Alive" and
+                    neighbors[2].condition == "Dead")):
+                    
+                    self._next_condition = "Alive"
+                    
+                    # 0 0 0
+                elif ((neighbors[0].condition == "Dead" and 
+                    neighbors[1].condition == "Dead" and
+                    neighbors[2].condition == "Dead") or
+                    # el y del vecino es igual al y de self -1 es arriba
+                    # 0 1 0
+                    (neighbors[0].condition == "Dead" and
+                    neighbors[1].condition == "Alive" and
+                    neighbors[2].condition == "Dead") or
+                    
+                    # 1 0 1
+                    (neighbors[0].condition == "Alive" and
+                    neighbors[1].condition == "Dead" and
+                    neighbors[2].condition == "Alive") or
+                    
+                    # 1 1 1
+                    (neighbors[0].condition == "Alive" and
+                    neighbors[1].condition == "Alive" and
+                    neighbors[2].condition == "Alive")):
+                    
+                    self._next_condition = "Dead"
+                    
+    
+    def advance(self):
+        """
+        Advance the model by one step.
+        """
+        if self._next_condition is not None:
+            self.condition = self._next_condition
---
+++ Actividad 1/Simulacion 2/model.py
@@ -0,0 +1,90 @@
+import mesa
+from mesa import Model, DataCollector
+from mesa.space import SingleGrid
+from mesa.time import SimultaneousActivation 
+
+from agent import TreeCell
+
+class AutomataCelular(Model):
+    """
+        Simple Forest Fire model.
+
+        Attributes:
+            height, width: Grid size.
+            density: What fraction of grid cells have a tree in them.
+            
+        Modelo de un Automata Celular
+        
+        Atributos:
+            height, width: tamaño del grid
+            density: porcentaje del grid que tiene cells
+    """
+
+    def __init__(self, height=50, width=50, density=0.65):
+        """
+        Create a new forest fire model.
+        
+        Args:
+            height, width: The size of the grid to model
+            density: What fraction of grid cells have a tree in them.
+        """
+
+        # Set up model objects
+        # SimultaneousActivation is a Mesa object that runs all the agents at the same time. 
+        # This is necessary in this model because the next state of each cell depends on the current state of all its neighbors -- before they've changed.
+        # This activation method requires that all the agents have a step() and an advance() method. 
+        # The step() method computes the next state of the agent, and the advance() method sets the state to the new computed state.
+        self.schedule = SimultaneousActivation(self)
+        self.grid = SingleGrid(height, width, torus=False)
+        self.i = 0
+
+        # A datacollector is a Mesa object for collecting data about the model.
+        # We'll use it to count the number of trees in each condition each step.
+        self.datacollector = DataCollector(
+            {
+                "Alive": lambda m: self.count_type(m, "Alive"),
+                "Dead": lambda m: self.count_type(m, "Dead")
+            }
+        )
+
+        # Place a tree in each cell with Prob = density
+        # coord_iter is an iterator that returns positions as well as cell contents.
+        for contents, (x, y) in self.grid.coord_iter():
+            if (self.random.random() < density):
+                # Create a tree
+                new_tree = TreeCell((x, y), self)
+                self.grid.place_agent(new_tree, (x, y))
+                self.schedule.add(new_tree)
+            else:
+                new_tree = TreeCell((x, y), self)
+                new_tree.condition = "Dead"
+                self.grid.place_agent(new_tree, (x, y))
+                self.schedule.add(new_tree)
+
+        self.running = True
+        self.datacollector.collect(self)
+
+    def step(self):
+        """
+        Have the scheduler advance each cell by one step
+        """
+        self.schedule.step()
+        # collect data
+        self.datacollector.collect(self)
+
+        self.i += 1
+        if self.i >= 49:
+            self.running = False
+
+
+    # staticmethod is a Python decorator that makes a method callable without an instance.
+    @staticmethod
+    def count_type(model, tree_condition):
+        """
+        Helper method to count trees in a given condition in a given model.
+        """
+        count = 0
+        for tree in model.schedule.agents:
+            if tree.condition == tree_condition:
+                count += 1
+        return count
