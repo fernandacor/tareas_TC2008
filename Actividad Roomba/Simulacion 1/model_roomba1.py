56dd0efb332f00547e73b1dad32ba132610acf5f
Fernanda Cantú <fercantu56@gmail.com>
2023-11-14 13:56:40 -0600

    dirty cells slider, no sirve todavía

--- Actividad Roomba/Simulacion 1/model_roomba1.py
+++ Actividad Roomba/Simulacion 1/model_roomba1.py
@@ -11,7 +11,7 @@ class RoombaModel(Model):
         N: Number of agents in the simulation
         height, width: The size of the grid to model
     """
-    def __init__(self, N, width, height, obstacles):
+    def __init__(self, N, width, height, obstacles, dirtycells):
         self.num_agents = N
         # Multigrid is a special type of grid where each cell can contain multiple agents.
         self.grid = MultiGrid(width,height,torus = False) 
@@ -28,10 +28,10 @@ class RoombaModel(Model):
         border = [(x,y) for y in range(height) for x in range(width) if y in [0, height-1] or x in [0, width - 1]]
 
         # Calculate the number of obstacles based on the slider value
-        num_obstacles = int(obstacles * width * height) # AGREGUE ESTO PARA LO DEL SLIDER
+        num_obstacles = int(obstacles * width * height)
         
-        # # Calculate the number of dirty cells based on the slider value      
-        # num_dirtycells = int(dirtycells * width * height) # AGREGUE ESTO PARA LO DEL SLIDER
+        # Calculate the number of dirty cells based on the slider value      
+        num_dirtycells = int(dirtycells * width * height)
         
         #Add obstacles to the grid in random positions
         for pos in border:
@@ -49,25 +49,26 @@ class RoombaModel(Model):
 
             self.grid.place_agent(a, pos)
         
-        # # Add dirty cells to the grid in random positions
-        # for i in range(num_dirtycells):
-        #     a = DirtyCellAgent(i, self)
-        #     self.schedule.add(a)
+        # Add dirty cells to the grid in random positions
+        for i in range(num_dirtycells):
+            a = DirtyCellAgent(i+1000, self)
+            self.schedule.add(a)
 
-        #     pos = (self.random.randrange(self.grid.width), self.random.randrange(self.grid.height))
+            pos = (self.random.randrange(self.grid.width), self.random.randrange(self.grid.height))
 
-        #     while (not self.grid.is_cell_empty(pos)):
-        #         pos = (self.random.randrange(self.grid.width), self.random.randrange(self.grid.height))
+            while (not self.grid.is_cell_empty(pos)):
+                pos = (self.random.randrange(self.grid.width), self.random.randrange(self.grid.height))
 
-        #     self.grid.place_agent(a, pos)
+            self.grid.place_agent(a, pos)
 
         # Function to generate random positions
-        pos_gen = lambda w, h: (self.random.randrange(w), self.random.randrange(h))
+        # pos_gen = lambda w, h: (self.random.randrange(w), self.random.randrange(h)) # esto hace que sea random
+        pos_gen = lambda w, h: (1, 1)
 
         # Add the agent to a random empty grid cell
-        for i in range(self.num_agents):
+        for j in range(self.num_agents):
 
-            a = RoombaAgent(i+1000, self) 
+            a = RoombaAgent(j+2000, self) 
             self.schedule.add(a)
 
             pos = pos_gen(self.grid.width, self.grid.height)
@@ -75,7 +76,7 @@ class RoombaModel(Model):
             while (not self.grid.is_cell_empty(pos)):
                 pos = pos_gen(self.grid.width, self.grid.height)
 
-            self.grid.place_agent(a, pos)
+            self.grid.place_agent(a, pos)        
         
         self.datacollector.collect(self)
 
--- Actividad Roomba/Simulacion 1/server_roomba1.py
+++ Actividad Roomba/Simulacion 1/server_roomba1.py
@@ -1,7 +1,7 @@
 from mesa.visualization import ModularServer, CanvasGrid, BarChartModule
 from mesa.visualization import Slider
 from model_roomba1 import RoombaModel
-from agent_roomba1 import ObstacleAgent
+from agent_roomba1 import ObstacleAgent, DirtyCellAgent
 
 def agent_portrayal(agent):
     if agent is None: return
@@ -14,13 +14,13 @@ def agent_portrayal(agent):
     
     if (isinstance(agent, ObstacleAgent)):
         portrayal["Color"] = "grey"
-        portrayal["Layer"] = 1
+        portrayal["Layer"] = 2
         portrayal["r"] = 0.2
     
-    # if (isinstance(agent, DirtyCellAgent)):
-    #     portrayal["Color"] = "darkgreen"
-    #     portrayal["Layer"] = 2
-    #     portrayal["r"] = 0.5
+    if (isinstance(agent, DirtyCellAgent)):
+        portrayal["Color"] = "darkgreen"
+        portrayal["Layer"] = 1
+        portrayal["r"] = 0.5
     
     return portrayal
 
@@ -28,10 +28,9 @@ model_params = {
     "N":1, 
     "width":15, 
     "height":15,
-    # default, inicial, final, step
-    # AGREGUE ESTO PARA EL SLIDER
+    # <nombre>: Slider(<nombre>, default, inicial, final, step)
     "obstacles": Slider("Obstacles", 0.15, 0.01, 0.5, 0.01),
-    # "dirtycells": Slider("Dirty Cells", 0.15, 0.01, 0.5, 0.01)
+    "dirtycells": Slider("Dirty Cells", 0.15, 0.01, 0.5, 0.01)
     }
 
 grid = CanvasGrid(agent_portrayal, 15, 15, 500, 500)
@@ -41,5 +40,5 @@ bar_chart = BarChartModule(
     scope="agent", sorting="ascending", sort_by="Steps")
 
 server = ModularServer(RoombaModel, [grid, bar_chart], "Roomba Model - Simulación 1", model_params)
-                
+        
 server.launch()
