44ffd66fa432bd608710a711ef043d0603e2d399
Fernanda Cantú <fercantu56@gmail.com>
2023-11-15 22:50:29 -0600

    ya sirve POR FIN

--- Tarea Modelo Llanta/llantasA01782232.py
+++ Tarea Modelo Llanta/llantasA01782232.py
@@ -1,79 +1,108 @@
-# Tarea CG 1- Modelo para Llantas
-# Fernanda Cantú A01782232
+# Tarea CG 1: Modelo de Llanta
+# Fernanda Cantu A01782232
 
+# Librerias a utilizar
 import math
 
-def definirParametros():
-    # Parámetros
-    lados = int(input("Ingresa número de lados: ") or lados == 8)
-    radio = float(input("Ingresa radio: ") or radio == 1.0)
-    ancho = float(input("Ingresa ancho: ") or ancho == 0.5)
-    
+# Función para definir los parametros de la llanta
+def parametros(lados = 8, radio = 1, ancho = 0.5):
+    lados = int(input("Ingresa el número de lados: ") or lados)
+    radio = float(input("Ingresa el radio: ") or radio)
+    ancho = float(input("Ingresa el ancho: ") or ancho)
+
     return lados, radio, ancho
 
-def calcularVertices(lados, radio, ancho):
-    # Listas para almacenar vértices y vectores normales
+# Función para calcular los vertices en base a los parametros
+def calculoVertices(lados, radio, ancho):
+    # Inicializar lista para almacenar los vertices
     vertices = []
-    caras = []
-    normales = []
-
-    # Calcular vértices
+    
+    # Iterar por el número de lados para calcular los vertices usando el angulo
     for i in range(lados):
-        angulo = 2 * math.pi * i / lados
-        vertices.append((radio * math.cos(angulo), radio * math.sin(angulo), 0))
-        vertices.append((radio * math.cos(angulo), radio * math.sin(angulo), ancho))
+        #lista.append(x, y, z)
+        vertices.append((radio * math.cos(math.radians(360/lados)*(i+1)), radio * math.sin(math.radians(360/lados)*(i+1)), 0))
+        vertices.append((radio * math.cos(math.radians(360/lados)*(i+1)), radio * math.sin(math.radians(360/lados)*(i+1)), ancho))
+    
+    return vertices
 
-    # Calcular caras
-    carafrente = len(vertices) - 2
-    caraatras = len(vertices) - 1
+# Función para calcular las caras en base a los parametros
+def calculoCaras(lados, vertices, ancho):
+    # Inicializar lista para almacenar las caras
+    caras = []
+    
+    # Origen de las caras
+    centroA = len(vertices)
+    centroB = centroA + 1
+    vertices.append((0, 0, 0))
+    vertices.append((0, 0, ancho))
 
+    # Cara "Frontal":
+    # Centro de cara A
+    # Generar cara A
     for i in range(0, lados * 2, 2):
-        caras.append((carafrente, i, (i + 2) % (lados * 2)))
+        caras.append((centroA, (i + 2) % (lados * 2), i))
 
+    # "Borde" de la llanta:
+    # Generar caras laterales
     for i in range(0, lados * 2, 2):
         caras.append((i, (i + 2) % (lados * 2), (i + 3) % (lados * 2), i + 1))
 
+    # Cara "Trasera":
+    # Centro de cara B
+    # Generar cara B
     for i in range(1, lados * 2, 2):
-        caras.append((caraatras, i, (i + 2) % (lados * 2)))
+        caras.append((centroB, i, (i + 2) % (lados * 2)))
 
-    # Calcular normales
+    return caras
+
+def calculoNormales(vertices, caras):
+    # Inicializar lista para almacenar las normales
+    normales = []
+    
+    # Iterar por las caras
     for cara in caras:
-        v1 = vertices[cara[0]]
-        v2 = vertices[cara[1]]
-        v3 = vertices[cara[2]]
+        # Calcular los vertices de cada cara 
+        if cara[0] < len(vertices):
+            i = vertices[cara[0]]
+            j = vertices[cara[1]]
+            k = vertices[cara[2]]
 
-        vector1 = (v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2])
-        vector2 = (v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2])
+            # Vectores a partir de los vertices
+            vect1 = (j[0] - i[0], j[1] - i[1], j[2] - i[2])
+            vect2 = (k[0] - i[0], k[1] - i[1], k[2] - i[2])
 
-        normal = (vector1[1]*vector2[2] - vector1[2]*vector2[1],
-                  vector1[2]*vector2[0] - vector1[0]*vector2[2],
-                  vector1[0]*vector2[1] - vector1[1]*vector2[0])
-
-        length = math.sqrt(normal[0]**2 + normal[1]**2 + normal[2]**2)
-        #normales.append((normal[0]/length, normal[1]/length, normal[2]/length))
-
-        if length != 0:
-            normales.append((normal[0]/length, normal[1]/length, normal[2]/length))
-        else:
-            normales.append((0,0,0))
-           
+            # Producto cruz para obtener la normal de la cara
+            productoCruz = (vect1[1] * vect2[2] - vect1[2] * vect2[1],
+                            vect1[2] * vect2[0] - vect1[0] * vect2[2],
+                            vect1[0] * vect2[1] - vect1[1] * vect2[0])
             
-    with open("Tarea Modelo Llanta/rueda_tarea1.obj", 'w') as archivo:
-        # Escribir vértices
+            # Normalizar la normal obtenida en el producto cruz
+            length = math.sqrt(productoCruz[0]**2 + productoCruz[1]**2 + productoCruz[2]**2)
+            normales.append((productoCruz[0]/length, productoCruz[1]/length, productoCruz[2]/length))
+    
+    return normales
+
+def generarArchivo(vertices, caras, normales):
+    with open("./Tarea Modelo Llanta/llanta_tarea1.obj", "w") as outputFile:
+        outputFile.write("# OBJ para llanta - A01782232\n")
+        outputFile.write("# Vertices: " + str(len(vertices)) + "\n")
         for vertice in vertices:
-            archivo.write(f'v {vertice[0]} {vertice[1]} {vertice[2]}\n')
-
-        # Escribir normales
+            outputFile.write("v {} {} {}\n".format(*vertice))
+        outputFile.write("# Normales: " + str(len(normales)) + "\n")
         for normal in normales:
-            archivo.write(f'vn {normal[0]} {normal[1]} {normal[2]}\n')
-
-        # Escribir caras
+            outputFile.write("vn {} {} {}\n".format(*normal))
+        outputFile.write("# Caras: " + str(len(caras)) + "\n")
         for cara in caras:
-            archivo.write('f ')
+            outputFile.write("f")
             for vertice in cara:
-                archivo.write(f'{vertice + 1}/{vertice + 1} ')
-            archivo.write('\n')
+                outputFile.write(" {}/{}".format(vertice + 1, vertice + 1))
+            outputFile.write("\n")
 
-# Llamada a la función
-lados, radio, ancho = definirParametros()
-calcularVertices(lados, radio, ancho)
+def main():
+    lados, radio, ancho = parametros()
+    vertices = calculoVertices(lados, radio, ancho)
+    caras = calculoCaras(lados, vertices, ancho)
+    normales = calculoNormales(vertices, caras)
+    generarArchivo(vertices, caras, normales)
+
+main()
