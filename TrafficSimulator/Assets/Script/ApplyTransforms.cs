6b6109be931c5a6afb15becb0a18cd2d52d6c27a
Fernanda Cantú <fercantu56@gmail.com>
2023-12-01 21:11:32 -0600

    codigo comentado :)

--- TrafficSimulator/Assets/Script/ApplyTransforms.cs
+++ TrafficSimulator/Assets/Script/ApplyTransforms.cs
@@ -1,10 +1,17 @@
+// Programa para aplicar matrices de transformacion a un coche y sus llantas
+// Fernanda Cantú Ortega - A01782232
+// 15 de noviembre de 2023
+// Corregido: 1 de diciembre de 2023
+
+// Líbrerias a utilizar
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
+// Clase para aplicar transformaciones
 public class ApplyTransforms : MonoBehaviour
 {
+    // Definir variables
     [SerializeField] Vector3 displacement;
     [SerializeField] float rotationSpeed;
     [SerializeField] AXIS rotationAxis;
@@ -13,28 +20,28 @@ public class ApplyTransforms : MonoBehaviour
     Vector3[] car_baseVertices;
     Vector3[] car_newVertices;
 
-    [SerializeField] Object[] wheels;
+    [SerializeField] GameObject[] wheels;
     Mesh wheelMesh;
     Vector3[][] wheel_baseVertices;
     Vector3[][] wheel_newVertices;
 
-    // Start is called before the first frame update
+    // Función para inicializar
     void Start()
     {
+        // Para los coches:
         // Conseguir mesh y vertices del coche, y copiar vertices originales
         carMesh = GetComponentInChildren<MeshFilter>().mesh;
         car_baseVertices = carMesh.vertices;
         car_newVertices = new Vector3[car_baseVertices.Length];
 
+        // Recorrer vertices base y guardarlos en los nuevos
         for (int i = 0; i < car_baseVertices.Length; i++) 
         {
             car_newVertices[i] = car_baseVertices[i];
         }
 
-        // wheelMesh = GetComponentInChildren<MeshFilter>().mesh;
-        // wheel_baseVertices = wheelMesh.vertices;
-        // wheel_newVertices = new Vector3[wheel_baseVertices.Length];
-
+        // Para las llantas:
+        // Obtener mesh y guardar vertices de cada llanta
         wheel_baseVertices = new Vector3[wheels.Length][];
         for (int i = 0; i < wheels.Length; i++) 
         {
@@ -42,18 +49,22 @@ public class ApplyTransforms : MonoBehaviour
             wheel_baseVertices[i] = wheelMesh.vertices;
         }
 
+        // Copiar vertices originales de las llantas
         wheel_newVertices = new Vector3[wheels.Length][];
-        for (int i = 0; i < wheels.Length; i++) {
+        for (int i = 0; i < wheels.Length; i++) 
+        {
             wheel_newVertices[i] = new Vector3[wheel_baseVertices[i].Length];
-            for (int j = 0; j < wheel_baseVertices[i].Length; j++) {
+            
+            for (int j = 0; j < wheel_baseVertices[i].Length; j++) 
+            {
                 wheel_newVertices[i][j] = wheel_baseVertices[i][j];
             }
         }
     }
 
+    // Función que se llama cada frame
     void Update()
     {
-        // Llamar cada frame
         DoTransform();
     }
 
@@ -74,7 +85,7 @@ public class ApplyTransforms : MonoBehaviour
                                        car_baseVertices[i].z,
                                        1);
 
-            car_newVertices[i] = move * temp;
+            car_newVertices[i] = move * temp; 
         }
         
         carMesh.vertices = car_newVertices;
@@ -89,8 +100,6 @@ public class ApplyTransforms : MonoBehaviour
             
             // Matriz para moverse y rodar
             Matrix4x4 composite = move * rotate;
-            // if (i > 0)
-            //     composite *= rotate;
 
             // Multiplicar cada vertice de las llantas por la matriz compuesta
             for (int j = 0; j < wheel_newVertices[i].Length; j++)
